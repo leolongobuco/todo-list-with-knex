a289ab115384f37a1e15b425b6c7d57c740a09e1
Léo <longobucoleonardo@gmail.com>
2023-10-16 19:55:57 -0300

    remove .env

--- src/api/cards/create-card/create-card.builder.ts
+++ src/api/cards/create-card/create-card.builder.ts
@@ -1,8 +1,4 @@
 import { CreateCardTypes } from "./create-card.types"
 import { CardTypes } from "../card.types"
 
-export namespace CreateCardBuilder {
-	export const response = (card: CardTypes.Card): CreateCardTypes.Response => {
-		return { id: card.id }
-	}
-}
+export namespace CreateCardBuilder {}
